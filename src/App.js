a51444f6bae3d28b80d45ec7719b3238d7cf9748
KantaPaul <pobon.paul1994@gmai.com>
2018-09-25 23:51:10 +0600

    file add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import classes from './assets/style.less';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className={classes.header}> Hello </h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
