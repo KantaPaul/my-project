9f4f112e561c9d78318ebb5594c91cadbfa058f9
KantaPaul <pobon.paul1994@gmai.com>
2018-10-01 23:11:30 +0600

    menu add

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -4,25 +4,32 @@ import bootstrap from '../assets/styles/framework/bootstrap.less';
 
 let Menu = () => {
   return (
-    <div className={[bootstrap.navbar, bootstrap.navbarLight].join(' ')}>
+    <div className={[bootstrap.navbarLight].join(' ')}>
       <div className={bootstrap.container}>
-        <ul className={[bootstrap.nav, bootstrap.navPills].join(' ')}>
-          <li>
-            <NavLink className={bootstrap.navLink} exact to="/" activeClassName={bootstrap.active}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-two">Page Two</NavLink>
-          </li>
-          <li>
-            <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-three">Page Three</NavLink>
-          </li>
-          <li>
-            <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-four">Page Four</NavLink>
-          </li>
-          <li>
-            <a className={bootstrap.navLink} target="_blank" href="https://github.com/KantaPaul/my-project">Project Code Git Repo</a>
-          </li>
-        </ul>
+        <div className={bootstrap.row}>
+          <div className={bootstrap.colLg8}>
+            <ul className={[bootstrap.nav, bootstrap.navPills].join(' ')}>
+              <li>
+                <NavLink className={bootstrap.navLink} exact to="/" activeClassName={bootstrap.active}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-two">Page Two</NavLink>
+              </li>
+              <li>
+                <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-three">Page Three</NavLink>
+              </li>
+              <li>
+                <NavLink className={bootstrap.navLink} activeClassName={bootstrap.active} to="/page-four">Page Four</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className={bootstrap.colLg4}>
+            <div className={bootstrap.textRight}>
+              <a className={[bootstrap.btn, bootstrap.btnPrimary, bootstrap.mr2].join(' ')} target="_blank" href="https://github.com/KantaPaul/my-project">Git Repo</a>
+              <a className={[bootstrap.btn, bootstrap.btnSecondary].join(' ')} target="_blank" href="https://drive.google.com/open?id=1p_Zulunkzhi8hP1YVnHwsqHgUF_6w1VT">Download</a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
